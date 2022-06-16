4cbfe43de6d8167e95d9ad2e4327282837ee4640
jo22i <99878853+jo22i@users.noreply.github.com>
2022-06-16 13:38:18 +0300

    Some Changes

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,8 +1,6 @@
 install(TARGETS example1 example2)
 include(InstallRequiredSystemLibraries)
 
-set(CPACK_DEBIAN_PACKAGE_PREDEPENDS "cmake >= 3.10")
 set(CPACK_DEBIAN_PACKAGE_VERSION 1.0.0)
-#set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
-
-include(CPack)
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "AndreyChemodurov")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "cmake >= 10")
